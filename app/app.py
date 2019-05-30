9e68afe4ddf6b825a0fb3b39ad68d8b8465db8ab
saurabh <saurabhs@winjit.com>
2019-05-30 17:03:09 +0530

    added response on predict and respective changes

--- app/app.py
+++ app/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import render_template
 import os
 from flask import request
+import logging
+from modules.classification import Sentiment
+
+logger = logging.getLogger('Sentiment Analysis')
 
 app = Flask(__name__)
 
@@ -9,8 +13,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/predict', methods = ['POST'])
+def prediction():
+    text = request.form['sentiment']
+    response = Sentiment().prediction(text)
+    return render_template('index.html', message=response)
+
 
 if __name__ == "__main__":
+    logger.info('starting flask server...')
     host = '0.0.0.0'
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 8000))
---
+++ app/modules/classification.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thus May 30 15:46:56 2019
+@author: saurabh
+"""
+
+class Sentiment:
+
+    def __init__(self):
+        pass
+
+    def prediction(self, text):
+
+        result = text
+        return result
